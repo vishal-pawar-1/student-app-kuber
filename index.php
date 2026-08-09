11ab360fc784fd372f70448c898ffa1000f889d0
vishal <pawarvishal267@gmail.com>
2026-08-09 15:42:40 +0530

    changes cicd-

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Registration</title>
+    <title>Student_ Registration</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
